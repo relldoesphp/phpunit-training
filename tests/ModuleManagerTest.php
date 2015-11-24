2a0fb7c3ccb4aa4ce8060dccb4576bec1941e95b
Michelangelo <dragonbe+github@gmail.com>
2015-11-24 22:58:04 +0100

    PHPUnit Training
    Testing legacy code with code coverage, setup and teardown

--- tests/ModuleManagerTest.php
+++ tests/ModuleManagerTest.php
@@ -4,6 +4,21 @@
  */
 class ModuleManagerTest extends \PHPUnit_Framework_TestCase
 {
+    protected function setUp()
+    {
+        parent::setUp();
+        ModuleManager::$modules_install = array ();
+    }
+
+    protected function tearDown()
+    {
+        ModuleManager::$modules_install = array ();
+        parent::tearDown();
+    }
+
+    /**
+     * @covers ModuleManager::include_install
+     */
     public function testReturnImmediatelyWhenModuleAlreadyLoaded()
     {
         $module = 'Foo_Bar';
@@ -11,5 +26,16 @@ class ModuleManagerTest extends \PHPUnit_Framework_TestCase
         $result = ModuleManager::include_install($module);
         $this->assertTrue($result);
         $this->assertCount(1, ModuleManager::$modules_install);
+    }
+
+    /**
+     * @covers ModuleManager::include_install
+     */
+    public function testLoadingNonExistingModuleIsNotExecuted()
+    {
+        $module = 'Foo_Bar';
+        $result = ModuleManager::include_install($module);
+        $this->assertFalse($result);
+        $this->assertCount(0, ModuleManager::$modules_install);
     }
 }
