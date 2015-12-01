a656696290ae62ab2d743f3ecdc0dc7974bdf057
Michelangelo <dragonbe+github@gmail.com>
2015-12-01 18:47:32 +0100

    PHPUnit Training
    Testing databases: mocking PDO to speed up tests (complete)

--- tests/Model/ContactMapperTest.php
+++ tests/Model/ContactMapperTest.php
@@ -56,4 +56,72 @@ class ContactMapperTest extends \PHPUnit_Framework_TestCase
         $this->assertSame($testData[0], $result[0]);
         $this->assertSame($testData[1], $result[1]);
     }
+
+    /**
+     * @covers \In2it\Phpunit\Model\ContactMapper::save
+     */
+    public function testInsertingNewContact()
+    {
+        $testData = new \stdClass();
+        $testData->contact_id = 1;
+        $testData->name = 'Foo';
+        $testData->address = 'Foo Bar 123';
+        $testData->zip = '12345';
+        $testData->city = 'Baz';
+        $testData->country = 'AN';
+        $testData->email = 'foo@bar.baz';
+        $testData->phone = '+1234567890';
+        $testData->mobile = '+9876543210';
+
+        $pdoStmt = $this->getMockBuilder('\\PDOStatement')
+            ->setMethods(array ('execute'))
+            ->getMock();
+        $pdoStmt->method('execute')
+            ->will($this->returnValue(true));
+        $pdo = $this->getMockBuilder('\\PDO')
+            ->setMethods(array ('prepare'))
+            ->setConstructorArgs(array ('sqlite::memory:'))
+            ->getMock();
+        $pdo->method('prepare')
+            ->will($this->returnValue($pdoStmt));
+
+        $contact = new Contact($testData);
+        $contactMapper = new ContactMapper($pdo);
+        $result = $contactMapper->save($contact->toArray());
+        $this->assertTrue($result);
+    }
+
+    /**
+     * @covers \In2it\Phpunit\Model\ContactMapper::save
+     */
+    public function testUpdatingExistingContact()
+    {
+        $testData = new \stdClass();
+        $testData->contact_id = 1;
+        $testData->name = 'Foo';
+        $testData->address = 'Foo Bar 123';
+        $testData->zip = '12345';
+        $testData->city = 'Baz';
+        $testData->country = 'AN';
+        $testData->email = 'foo@bar.baz';
+        $testData->phone = '+1234567890';
+        $testData->mobile = '+9876543210';
+
+        $pdoStmt = $this->getMockBuilder('\\PDOStatement')
+            ->setMethods(array ('execute'))
+            ->getMock();
+        $pdoStmt->method('execute')
+            ->will($this->returnValue(true));
+        $pdo = $this->getMockBuilder('\\PDO')
+            ->setMethods(array ('prepare'))
+            ->setConstructorArgs(array ('sqlite::memory:'))
+            ->getMock();
+        $pdo->method('prepare')
+            ->will($this->returnValue($pdoStmt));
+
+        $contact = new Contact($testData);
+        $contactMapper = new ContactMapper($pdo);
+        $result = $contactMapper->save($contact->toArray());
+        $this->assertTrue($result);
+    }
 }
